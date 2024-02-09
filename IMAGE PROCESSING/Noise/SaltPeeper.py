7bcb7f8c62446c8f1517ebf623c513678c780f11
Arkya Ray <113771514+arkyaray2002@users.noreply.github.com>
2024-02-09 12:43:15 +0530

    Update SaltPeeper.py

--- IMAGE PROCESSING/Noise/SaltPeeper.py
+++ IMAGE PROCESSING/Noise/SaltPeeper.py
@@ -1,19 +1,19 @@
 import cv2
 import numpy as np
 
-def add_salt_and_paper(img , salt,paper):
+def add_salt_and_peeper(img , salt,peeper):
     noisy=np.copy(img)
 
     salt_pixel=np.random.rand(*img.shape)<salt
     noisy[salt_pixel]=255
 
-    paper_pixel=np.random.rand(*img.shape)<paper
-    noisy[paper_pixel]=0
+    peeper_pixel=np.random.rand(*img.shape)<peeper
+    noisy[peeper_pixel]=0
     return noisy
 
 org_img=cv2.imread("apple.jpg",0)
 salt=0.02
-paper=0.02
-noisy_img=add_salt_and_paper(org_img,salt,paper)
+peeper=0.02
+noisy_img=add_salt_and_peeper(org_img,salt,peeper)
 cv2.imshow("Noisy Image",noisy_img)
 cv2.waitKey(0)
