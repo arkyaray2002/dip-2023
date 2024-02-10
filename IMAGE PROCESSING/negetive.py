da13e901011724a3bd479ec209a5cccadbc6aee3
Arkya Ray <113771514+arkyaray2002@users.noreply.github.com>
2024-02-10 08:41:57 +0530

    Delete IMAGE PROCESSING directory

--- IMAGE PROCESSING/negetive.py
+++
@@ -1,8 +0,0 @@
-import cv2 
-img=cv2.imread('Taj.jpg') 
-height,width=img.shape[:2] 
-for i in range(height): 
- for j in range(width): 
-  img[i][j]=255-img[i][j] 
-cv2.imshow('neg', img) 
-cv2.waitKey(0) 
